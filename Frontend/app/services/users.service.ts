e701fd3c5165ece841b3aed12dbce5a5b7d40ba5
Raul Ribeiro <a22552@alunos.ipca.pt>
2026-04-01 14:37:49 +0100

    fix: Update API URL references in AuthService and UsersService for consistency

--- Frontend/app/services/users.service.ts
+++ Frontend/app/services/users.service.ts
@@ -1,4 +1,6 @@
 export class UsersService {
+    private _apiUrl = 'http://localhost:3000';
+
     /**
      * Bloqueia um utilizador com base no seu ID.
      * 
@@ -6,7 +8,7 @@ export class UsersService {
      * @returns A resposta da API após tentar bloquear o utilizador.
      */
     async blockUser(userId: number) {
-        const response = await fetch(`http://localhost:3000/utilizador/${userId}/block`, {
+        const response = await fetch(`${this._apiUrl}/utilizador/${userId}/block`, {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
         });
@@ -21,7 +23,7 @@ export class UsersService {
      * @returns A resposta da API após tentar desbloquear o utilizador.
      */
     async unlockUser(userId: number) {
-        const response = await fetch(`http://localhost:3000/utilizador/${userId}/unlock`, {
+        const response = await fetch(`${this._apiUrl}/utilizador/${userId}/unlock`, {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
         });
@@ -35,7 +37,7 @@ export class UsersService {
      * @returns A resposta da API após tentar obter os utilizadores.
      */
     async getUsers() {
-        const response = await fetch(`http://localhost:3000/utilizador`, {
+        const response = await fetch(`${this._apiUrl}/utilizador`, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         });
