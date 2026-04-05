45d51b9832ca723fae70cec97db3cee16b1242dc
mepe07 <pedrorafaelmendes1993@gmail.com>
2026-04-05 20:08:22 +0100

    Update coaching.service.ts

--- Backend/src/coaching/coaching.service.ts
+++ Backend/src/coaching/coaching.service.ts
@@ -8,27 +8,11 @@ export class CoachingService {
 
   constructor(private readonly prisma: PrismaService) {} // Injeta o PrismaService no construtor
   
+
   async create(createCoachingDto: CreateCoachingDto) {
     // Usa o Prisma para criar um novo registo de coaching na DB
     return this.prisma.coaching.create({
       data: createCoachingDto, // Os dados para criar o coaching vêm do DTO
     });
   }
-
-  // async findAll() {
-  //   // Usa o Prisma para ir buscar todos os registos de coaching na DB
-  //   return this.prisma.coaching.findMany();
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} coaching`;
-  // }
-
-  // update(id: number, updateCoachingDto: UpdateCoachingDto) {
-  //   return `This action updates a #${id} coaching`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} coaching`;
-  // }
 }
