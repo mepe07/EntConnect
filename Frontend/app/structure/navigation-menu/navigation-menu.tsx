df2e4c2791dde15d16df8d2d81cb63aecb4cafd3
mepe07 <pedrorafaelmendes1993@gmail.com>
2026-04-20 19:00:56 +0100

    update nabigation menu

--- Frontend/app/structure/navigation-menu/navigation-menu.tsx
+++ Frontend/app/structure/navigation-menu/navigation-menu.tsx
@@ -190,7 +190,9 @@ export function NavigationMenu() {
     return (
         <nav className="navigation-menu">
             <ul>
-                {menuAtivo.map((item, index) => {
+                {/* {menuAtivo.map((item, index) => { */}
+                {(menuAtivo || []).map((item, index) => {
+                    
                     
                     // Caso A: Item de navegação simples (sem submenu)
                     if (item.path && !item.submenu) {
